25c7e68fbab873b2c6d8273bfe76785fd7cc3160
SimonVutov <SimonVutov1@gmail.com>
2023-09-14 22:38:11 -0400

    Assignment1
    Assignment1

--- 101QuizGamereviewof3Uconcepts.cpp
+++ 101QuizGamereviewof3Uconcepts.cpp
@@ -6,30 +6,105 @@
 #include <string>
 using namespace std;
 
-void answerPrint(char answer) {
-    cout << "\nEnter your answer: ";
+// Define color escape sequences
+const string RED_COLOR = "\033[7;31m";
+const string GREEN_COLOR = "\033[1;32m";
+const string YELLOW_COLOR = "\033[33m";
+const string BLUE_COLOR = "\033[34m";
+const string DARK_GRAY_COLOR = "\033[1;30m";
+const string RESET_COLOR = "\033[0m";
+
+// Global variables
+int QuestionsNumber = 1;
+int correctAnswers = 0;
+int totalQuestions = 0;
+
+string getInput() {
     string ans;
     cin >> ans;
     ans[0] = tolower(ans[0]); //make lowercase
     //check if valid answer
     while (ans[0] != 'a' && ans[0] != 'b' && ans[0] != 'c' && ans[0] != 'd' || ans.length() > 1) {
-        cout << "Invalid answer, please enter a, b, c, or d: ";
+        cout << YELLOW_COLOR << "Invalid answer" << RESET_COLOR << ", please enter a, b, c, or d: ";
         cin >> ans;
         ans[0] = tolower(ans[0]);
     }
-    cout << (ans[0] == answer ? "Correct!" : "Incorrect!, the correct answer is " + string(1, answer)) << endl;
+    return ans;
+}
+
+void printAnswer(char answer) {
+    cout << DARK_GRAY_COLOR << "\nEnter your answer: " << RESET_COLOR;
+    string ans = getInput();
+    if (ans[0] == answer) {
+        cout << GREEN_COLOR << "Correct!" << RESET_COLOR << endl;
+        correctAnswers++;
+    } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << string(1, answer) << endl;
+    totalQuestions++;
 }
 
 void questionPrint(string question, string a, string b, string c, string d, char answer) {
+    cout << BLUE_COLOR << "\nQuestion " << QuestionsNumber++ << ":" << RESET_COLOR << endl;
     cout << question << endl;
     cout << "a) " << a << endl;
     cout << "b) " << b << endl;
     cout << "c) " << c << endl;
     cout << "d) " << d << endl;
-    answerPrint(answer);
+    printAnswer(answer);
 }
 
 int main () {
+    cout << "Type the value that will be outputted by the following code: " << endl;
+    cout << "cout << 5 / 2 + 1 << endl;" << endl;
+    int ans = 5 / 2 + 1;
+    cout << DARK_GRAY_COLOR << "Enter your answer: " << RESET_COLOR;
+    int input;
+    cin >> input;
+    if (input == ans) {
+        cout << GREEN_COLOR << "Correct!" << RESET_COLOR << endl;
+        correctAnswers++;
+    } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << ans << endl;
+    totalQuestions++;
+
+
+
+    cout << "\nWhat is the output of the following code?" << endl;
+    cout << "cout << \"hi\" + 10 + \"bye\" << endl;" << endl;
+    string ans2 = "hi10bye";
+    cout << DARK_GRAY_COLOR << "Enter your answer: " << RESET_COLOR;
+    string input2;
+    cin >> input2;
+    if (input2 == ans2) {
+        cout << GREEN_COLOR << "Correct!" << RESET_COLOR << endl;
+        correctAnswers++;
+    } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << ans2 << endl;
+    totalQuestions++;
+
+
+
+    cout << "\nType 'a', 'b', 'c', or 'd' to answer the questions." << endl;
+
+
+    questionPrint("Which of these rounds a number up to the nearest integer?",
+    "floor()",
+    "ceil()",
+    "round()",
+    "trunc()",
+    'b');
+
+    questionPrint("What is the purpose of overloading a method?",
+    "To make the method more efficient by reducing the number of lines of code",
+    "To make the method more readable by simplifying the code",
+    "To make the method more flexible by allowing it to take in different types of parameters",
+    "To make the method more accurate",
+    'c');
+
+    questionPrint("Which of these takes the absolute value of a number?",
+    "abs()",
+    "max()",
+    "positive()",
+    "val()",
+    'a');
+
     questionPrint("What is written after std::cout to print a string?",
     "<<",
     ">>",
@@ -127,4 +202,22 @@ int main () {
     "cin >> input;",
     "cin << input >> endl;",
     'c');
+
+    questionPrint("How do you overload a method?",
+    "By creating a method with different parameters",
+    "By creating a method with different return type",
+    "By creating a method with different parameters and return type",
+    "By creating a method with different parameters and different return type",
+    'a');
+
+    questionPrint("How do you make a boolean array of size 10, called visited, with all values initialized to false?",
+    "bool visited[10] = false;",
+    "bool visited[10];",
+    "bool visited[10] = {0};",
+    "bool visited[10] = {false};",
+    'c');
+
+
+    cout << " You correctly answered " << correctAnswers << " out of " << totalQuestions << " questions!" << endl;
+    return 0;
 }
