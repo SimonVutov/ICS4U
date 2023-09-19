38e8e86956153e38ea6f156219597771e25b4976
SimonVutov <SimonVutov1@gmail.com>
2023-09-19 14:11:56 -0400

    Assignment 1
    Assignment 1

--- 101QuizGamereviewof3Uconcepts.cpp
+++ 101QuizGamereviewof3Uconcepts.cpp
@@ -17,12 +17,12 @@ const string RESET_COLOR = "\033[0m";
 // Global variables
 int QuestionsNumber = 1;
 int correctAnswers = 0;
-int totalQuestions = 0;
 
+// takes in input, checks if valid, returns input
 string getInput() {
     string ans;
     cin >> ans;
-    ans[0] = tolower(ans[0]); //make lowercase
+    ans[0] = tolower(ans[0]); // make lowercase
     //check if valid answer
     while (ans[0] != 'a' && ans[0] != 'b' && ans[0] != 'c' && ans[0] != 'd' || ans.length() > 1) {
         cout << YELLOW_COLOR << "Invalid answer" << RESET_COLOR << ", please enter a, b, c, or d: ";
@@ -32,6 +32,7 @@ string getInput() {
     return ans;
 }
 
+// takes in correct answer, checks if user input is correct, prints correct or incorrect
 void printAnswer(char answer) {
     cout << DARK_GRAY_COLOR << "\nEnter your answer: " << RESET_COLOR;
     string ans = getInput();
@@ -39,50 +40,178 @@ void printAnswer(char answer) {
         cout << GREEN_COLOR << "Correct!" << RESET_COLOR << endl;
         correctAnswers++;
     } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << string(1, answer) << endl;
-    totalQuestions++;
 }
 
+// prints question, options, and calls printAnswer
 void questionPrint(string question, string a, string b, string c, string d, char answer) {
     cout << BLUE_COLOR << "\nQuestion " << QuestionsNumber++ << ":" << RESET_COLOR << endl;
     cout << question << endl;
-    cout << "a) " << a << endl;
-    cout << "b) " << b << endl;
-    cout << "c) " << c << endl;
-    cout << "d) " << d << endl;
+    cout << "a) " << a << "\nb) " << b << "\nc) " << c << "\nd) " << d << endl;
     printAnswer(answer);
 }
 
-int main () {
-    cout << "Type the value that will be outputted by the following code: " << endl;
-    cout << "cout << 5 / 2 + 1 << endl;" << endl;
-    int ans = 5 / 2 + 1;
+// takes in question and answer, checks if user input is correct, prints correct or incorrect
+void stringQuestion(string question, string answer) {
+    cout << BLUE_COLOR << "\nQuestion " << QuestionsNumber++ << ":" << RESET_COLOR << endl;
+    cout << question << endl;
     cout << DARK_GRAY_COLOR << "Enter your answer: " << RESET_COLOR;
-    int input;
+    string input;
     cin >> input;
-    if (input == ans) {
+    if (input == answer) {
         cout << GREEN_COLOR << "Correct!" << RESET_COLOR << endl;
         correctAnswers++;
-    } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << ans << endl;
-    totalQuestions++;
+    } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << answer << endl;
+}
 
+int main () {
+    cout << "Welcome to the best c++ quiz. There are 40 questions!! \nSome questions will have multiple choice answers, others will require you to type in the answer." << endl;
 
+    questionPrint("What is the output of the following code? for (int i = 0; i < 10; i++) { cout << i; }",
+    "1 2 3 4 5 6 7 8 9 10",
+    "0 1 2 3 4 5 6 7 8 9",
+    "0123456789",
+    "12345678910",
+    'c');
 
-    cout << "\nWhat is the output of the following code?" << endl;
-    cout << "cout << \"hi\" + 10 + \"bye\" << endl;" << endl;
-    string ans2 = "hi10bye";
-    cout << DARK_GRAY_COLOR << "Enter your answer: " << RESET_COLOR;
-    string input2;
-    cin >> input2;
-    if (input2 == ans2) {
-        cout << GREEN_COLOR << "Correct!" << RESET_COLOR << endl;
-        correctAnswers++;
-    } else cout << RED_COLOR << "Incorrect! " << RESET_COLOR << "The correct answer is " << ans2 << endl;
-    totalQuestions++;
+    questionPrint("Which of these is not a valid way of initializing an integer?",
+    "int a = 1;",
+    "int a(1);",
+    "int a = {1};",
+    "int a{1};",
+    'c');
 
+    //auto and decltype
+    questionPrint("What does the 'auto' keyword in C++ primarily mean?",
+    "Implicit type conversion",
+    "Dynamic memory allocation",
+    "Type deduction",
+    "Operator overloading",
+    'c');
 
+    //fundamental data types
+    questionPrint("Which of these is not a fundamental data type?",
+    "int",
+    "float",
+    "double",
+    "string",
+    'd');
 
-    cout << "\nType 'a', 'b', 'c', or 'd' to answer the questions." << endl;
+    questionPrint("Which of this is used to print a new line?",
+    "\\n",
+    "\\t",
+    "\\r",
+    "\\a",
+    'a');
 
+    //cout << "hello" "world \n";
+    questionPrint("What is the output of the following code? cout << \"hello\" \"world \\n\";",
+    "\"helloworld\" and a newline",
+    "hello world",
+    "hello world\\n",
+    "hello world\\n\\n",
+    'a');
+
+    //const vs define
+
+    //different between ++x and x++
+    //x++ returns the value of x before it is incremented
+    //++x returns the value of x after it is incremented
+    questionPrint("What is printed? int x = 5; cout << ++x; cout << x++;",
+    "56",
+    "66",
+    "65",
+    "55",
+    'b');
+
+    //const double pi = 3.14;
+    //x=y=z=0
+    stringQuestion("What value does x have? y=z=4; x=y+z;", "8");
+
+    //&&, ||, !, ==
+    questionPrint("Which of these is the correct way to check if x is the same as y or z?",
+    "x == y || z",
+    "x == y && z",
+    "x == y || x == z",
+    "x == y && x == z",
+    'c');
+
+    //7==5 ? 4 : 3
+    //which returns the larger number? return a > b ? a : b;
+    questionPrint("Which of these does NOT return the larger number?",
+    "return a > b ? a : b;",
+    "return max(a, b)",
+    "return a > b ? b : a;",
+    "if (a > b) return a; else return b;",
+    'c');
+
+    //is this the same
+    //cin>>a>>b;
+    //cin>>a;
+    //cin>>b;
+    stringQuestion("Is the following code the same? \"cin>>a>>b;\" and \"cin>>a; cin>>b;\", type yes or no", "yes");
+
+    //cin vs getline
+    questionPrint("What is the difference between cin and getline?",
+    "cin takes input up to a space, getline takes input up to a new line",
+    "cin takes input up to a new line, getline takes input up to a space",
+    "cin takes in a char, getline takes in a string",
+    "cin takes in a string, getline takes in a char",
+    'a');
+
+    //template
+    questionPrint("What is the purpose of the following C++ code?\n"
+    "template <typename T>\n"
+    "void swap(T& a, T& b) {\n"
+    "    T temp = a;\n"
+    "    a = b;\n"
+    "    b = temp;\n"
+    "}\n",
+    "It calculates the average of two numbers",
+    "It swaps the values of two variables, regardless of their type",
+    "It checks if two variables are equal",
+    "It calculates the product of two numbers",
+    'b');
+
+    //stringstream, to_string, stoi, stod
+    stringQuestion("What is the value of ss.str() after the following code? stringstream ss; ss << 5 << 10; ss << hi;", "510hi");
+
+    // for (n=0, i=100; n!=1; ++n, --i) {} //more than one condition
+    questionPrint("When does this for loop run? for (n=0, i=100; n!=1; ++n, --i) {}",
+    "When n is not equal to 1",
+    "When n is equal to 1",
+    "When i is equal to 1",
+    "When i is not equal to 1",
+    'a');
+
+    //range based for loop, //for (char c : str) {cout << c << endl;}
+    questionPrint("What is an example of a range based for loop?",
+    "for (int i = 0; i < 10; i++)",
+    "for (int i : v)",
+    "for (int i = 0; i < v.size(); i++)",
+    "for (int i = 0; i < v.size(); i++)",
+    'b');
+
+    //break;, continue;, goto;, return;
+    stringQuestion("You want to end the skip this iteration in a loop, which of these do you use? Type break, continue, goto, return", "continue");
+
+    //switch statement
+    questionPrint("Which scenario is most likely to use a switch statement?",
+    "When you want to check if a number is even or odd",
+    "When you want to check if a number is positive or negative",
+    "When you want to check if a number is 1, 2, or 3, and do different things for each",
+    "When there is a vector of numbers to output",
+    'c');
+
+    //default perameters int devide (int a, int b=1) // devuide(12) is 12/2
+    questionPrint("Default perameters are used for which of these?",
+    "To make the code more readable",
+    "To allow the user to input some or all of the perameters in a function",
+    "To overload the function",
+    "To let the user know what the function does",
+    'b');
+
+    stringQuestion("Type the value that will be outputted by the following code: cout << (5/2+1)", "3");
+    stringQuestion("What is the output of the following code? cout << \"hi\" + 10 + \"bye\" << endl;", "hi10bye");
 
     questionPrint("Which of these rounds a number up to the nearest integer?",
     "floor()",
@@ -210,6 +339,14 @@ int main () {
     "By creating a method with different parameters and different return type",
     'a');
 
+    //unsigned and signed
+    questionPrint("Which of these is FALSE about unsigned integers?",
+    "They can only be positive",
+    "They can store a larger range of positive numbers than signed ints",
+    "They don't have a bit reserved for the sign",
+    "They have a larger positive range than signed ints",
+    'b');
+
     questionPrint("How do you make a boolean array of size 10, called visited, with all values initialized to false?",
     "bool visited[10] = false;",
     "bool visited[10];",
@@ -217,7 +354,6 @@ int main () {
     "bool visited[10] = {false};",
     'c');
 
-
-    cout << " You correctly answered " << correctAnswers << " out of " << totalQuestions << " questions!" << endl;
+    cout << " You correctly answered " << correctAnswers << " out of " << QuestionsNumber - 1 << " questions." << endl;
     return 0;
 }
